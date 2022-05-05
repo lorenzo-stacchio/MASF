c4e31a9446745767c7b51273c908d077cb31057d
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2022-05-05 16:21:50 +0200

    random biometrics value

--- Final_project_XR/Assets/Scripts/media_manager.cs
+++ Final_project_XR/Assets/Scripts/media_manager.cs
@@ -3,24 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Metrics{
+    bpm,
+    mmHg, 
+    spO2,
+    rpm,
+    temperature,
+}
+
+
 public class media_manager : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    [SerializeField] Image eeg; // eeg simulation
-    [SerializeField] Image bpm; // heart rate
-    [SerializeField] Image brm; // breath
-
+    //[SerializeField] Text value; // eeg simulation
+    [SerializeField] Metrics info_to_reproduce;
 
 
     void Start()
     {
-        
+        InvokeRepeating("updateInfo", 5.0f, 1);
     }
-
+    
     // Update is called once per frame
-    void Update()
+    void updateInfo()
     {
-        
+        // this script hypotesize the existence of a children named value
+        var text_component = GetComponentInChildren<Text> ();
+        switch (info_to_reproduce)
+        {
+        case Metrics.bpm:
+            //50 e 120 
+            text_component.text = Mathf.Round(Random.Range(50, 120)).ToString() + " bpm";
+            break;
+        case Metrics.rpm:
+           // 16 e 20 al minuto
+            text_component.text = Mathf.Round(Random.Range(16, 20)).ToString()+ " rpm";
+            break;
+        case Metrics.mmHg:
+           //90 160 
+            var min = Mathf.Round(Random.Range(70, 110));  
+            var max = Mathf.Round(Random.Range(140, 180));  
+            text_component.text = string.Format("{0}\n{1}\n({2})", min.ToString(), max.ToString() , (int)(min+max)/2).ToString();
+            break;
+        case Metrics.spO2:
+           //90 100
+            text_component.text = Mathf.Round(Random.Range(90, 100)).ToString() + " %";
+            break;
+        default: // temperature
+            //36,39
+            text_component.text = Mathf.Round(Random.Range(36, 39)).ToString()+ " °C";;
+            Debug.Log("No default");
+            break;
+        }
     }
 }
