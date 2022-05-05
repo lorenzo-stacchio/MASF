1b53d201952e1a02c36a3ca75f466e42bb9526c0
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2022-05-05 15:44:04 +0200

    panel with icons

---
+++ Final_project_XR/Assets/Scripts/media_manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class media_manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Image eeg; // eeg simulation
+    [SerializeField] Image bpm; // heart rate
+    [SerializeField] Image brm; // breath
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
